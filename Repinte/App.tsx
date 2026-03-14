24fc63cb0beb61e13a381dea763c5d2df13ee8fd
rk-sophia <soph62217@gmail.com>
2026-03-13 21:54:48 -0300

    logo inserida, menu criado, atualizações de versão pra renderizar certo

--- Repinte/App.tsx
+++ Repinte/App.tsx
@@ -1,80 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, ScrollView} from 'react-native';
-import { useVideoPlayer, VideoView } from 'expo-video';
 
-export default function App() {
+//Menu
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// Ícones
+import Ionicons from '@expo/vector-icons/Ionicons';
+//Menu SOBRE
+import telaSobre from "./telas/Sobre"
 
-  //India o video e coloca ele em loop
-  const player = useVideoPlayer('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', player => {
-    player.loop = true
-    //player.play() inicia o video quando abre o app
-  })
 
-  return (
-  <ScrollView contentContainerStyle={styles.container}>
-  <Text style={styles.titulo}>Salão do Francis</Text>
-  
-  <Text style={styles.textoCorpo}>
-    Com a franquia da maior especialista em restauração predial do mercado, o seu retorno pode acontecer no 1° ano com até 40% de lucratividade!
+//Menu  - BOTTOM TABS
+const Tab = createBottomTabNavigator();
+function Menu(){
+  return <Tab.Navigator
+              screenOptions={(route)=>({
+                  tabBarIcon:({focused, color, size})=>{
+                    let iconName: any;
 
-    {"\n\n"}
-    Você pode iniciar Home Based
-
-    Descansa nos finais de semana
-
-    A obra é executada a partir do financiamento do próprio cliente
-  </Text>
-
-  <Image 
-  source={require('./assets/imagem 1.jpg')} 
-  style={styles.imagemInicio} 
-  />
-
-  <StatusBar style="auto" />
-  
-   <VideoView player ={player} style ={styles.video}
-    allowsPictureInPicture/>
-
-</ScrollView>
-  );
+                    if(route.name==="Sobre"){
+                      iconName = focused ? 'paw' : 'paw-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color}/>
+                  },
+                  headerShown: false,
+                  tabBarActiveTintColor: 'blue',
+                  tabBarInactiveTintColor: 'orange',
+                })}
+              >
+                <Tab.Screen name="Sobre" component={telaSobre}/>
+                <Tab.Screen name="Sobre2" component={telaSobre}/>
+                <Tab.Screen name="Sobre3" component={telaSobre}/>
+                <Tab.Screen name="Sobre4" component={telaSobre}/>
+        </Tab.Navigator>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    //flex: 1,
-    backgroundColor: '#f8cf76ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 25, // Importante para o texto não bater nas bordas
-  },
-  titulo: {
-    marginTop:35,
-    fontSize: 26,
-    fontWeight: 'bold',
-    marginBottom: 15,
-    color: '#333',
-  },
-  textoCorpo: {
-    fontSize: 16,
-    lineHeight: 24, // Dá espaço entre as linhas
-    textAlign: 'justify',
-    color: '#444',
-  },
+export default function App() {
+  return <NavigationContainer>
+            <Menu />
+        </NavigationContainer>
 
-  imagemInicio: {
-  width: 500,
-  height: 250,
-  resizeMode: 'contain', // Isso garante que a imagem não fique esticada
-  marginTop: 40,
-  marginBottom: 50,
-},
-
-  video: {
-    width: 350,
-    height: 275,
-    alignSelf: "center",
-  },
-  controlsContainer: {
-    padding: 10,
-  },
-});
+}
---
+++ Repinte/telas/Sobre.tsx
@@ -0,0 +1,81 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Image, ScrollView} from 'react-native';
+import { useVideoPlayer, VideoView } from 'expo-video';
+
+
+export default function App() {
+
+  //India o video e coloca ele em loop
+  const player = useVideoPlayer('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', player => {
+    player.loop = true
+    //player.play() inicia o video quando abre o app
+  })
+
+  return (
+  <ScrollView contentContainerStyle={styles.container}>
+  <Text style={styles.titulo}>Sobre a Repinte</Text>
+  
+  <Text style={styles.textoCorpo}>
+    Com a franquia da maior especialista em restauração predial do mercado, o seu retorno pode acontecer no 1° ano com até 40% de lucratividade!
+
+    {"\n\n"}
+    Você pode iniciar Home Based
+
+    Descansa nos finais de semana
+
+    A obra é executada a partir do financiamento do próprio cliente
+  </Text>
+
+  <Image 
+  source={require('../assets/imagem.jpg')} 
+  style={styles.imagemInicio} 
+  />
+
+  <StatusBar style="auto" />
+  
+   <VideoView player ={player} style ={styles.video}
+    allowsPictureInPicture/>
+
+</ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+    backgroundColor: '#f8cf76ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 25, // Importante para o texto não bater nas bordas
+  },
+  titulo: {
+    marginTop:35,
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    color: '#333',
+  },
+  textoCorpo: {
+    fontSize: 16,
+    lineHeight: 24, // Dá espaço entre as linhas
+    textAlign: 'justify',
+    color: '#444',
+  },
+
+  imagemInicio: {
+  width: 500,
+  height: 250,
+  resizeMode: 'contain', // Isso garante que a imagem não fique esticada
+  marginTop: 40,
+  marginBottom: 50,
+},
+
+  video: {
+    width: 350,
+    height: 275,
+    alignSelf: "center",
+  },
+  controlsContainer: {
+    padding: 10,
+  },
+});
